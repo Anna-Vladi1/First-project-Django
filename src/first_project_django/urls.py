2ecbae539398bd6332f68dcd72859004b821247e
Vladimirova Anna <niura.vladimirova@yandex.ru>
2025-04-25 13:33:47 +0300

    delete all

--- src/first_project_django/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('rooms_app.urls')), # <--- включаем urls нашего приложения
-
-]
